65605118b2898f9a6ccec4ae6ac0863c994ee403
Zerdardian | Kjeld Jense <legokjeld@hotmail.com>
2022-11-18 10:50:48 +0100

    Start and base of the project

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+
+    // Composer load
+    include_once "/vendor/autoload.php";
+    // Setup basis preloaded files
+    include_once "/assets/php/basis.php";
+    // Setup core
+    include_once "/assets/php/core.php";
