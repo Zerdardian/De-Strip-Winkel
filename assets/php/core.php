d6a0cc83ef61c9ac3c84f63181541f29aae390b5
Zerdardian | Kjeld Jense <legokjeld@hotmail.com>
2022-11-18 11:21:56 +0100

    added info for adding.

--- assets/php/core.php
+++ assets/php/core.php
@@ -1,11 +1,17 @@
 <?php
     if(!empty($db)) {
         // Posts loaded if page is known to the system and will be set here based on function / page
+        // Add functions with they required items through /posts/(they own map.)
+        // Example: /posts/user/basis.php or /posts/user/userinfo.php
         if(!empty($_POST)) {
             include_once "./assets/posts/settings.php";
         }
 
         // Building the webpage
+
+        // Creation of new pages or functions for the webpage.
+        // Please add them in /pages/functions/ or /pages/singepages/
+        // For single pages, they will be loaded instantly while Functions require they own items to be created through there.
 
         if(!empty($page)) {
             // Check if there is a function for this kind of page
