1a7c8cbcc0499181461b773969e484a956e14dd5
Zerdardian | Kjeld Jense <legokjeld@hotmail.com>
2022-11-21 10:54:55 +0100

    added text

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
     session_start();
 
+    /**
+     * Before adding or changing things. Please read.
+     * Adding pages can be done in the /assets/pages/. For a single page, /singlepages/
+     * For functions and multiple pages, /functions/ pages. Continue writing page functions through there as well so they are always loaded when required.
+     * 
+     * Add related data in .env or the .env.example if required!
+     * 
+     * Thank you for reading.
+     */
+
     // Composer load
     include_once "./vendor/autoload.php";
     // Setup basis preloaded files
