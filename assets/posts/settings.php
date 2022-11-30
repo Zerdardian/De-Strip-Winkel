6f6d8e8077a8540b02a3cadc1199f985d5296d5c
Zerdardian | Kjeld Jense <legokjeld@hotmail.com>
2022-11-30 11:24:43 +0100

    woops

--- assets/posts/settings.php
+++ assets/posts/settings.php
@@ -7,18 +7,24 @@
             if(!empty($pageitem)) {
                 // Check if the function is known
                 if(file_exists('./assets/posts/functions/'.$page.'/'.$pageitem.'.php')) {
+                    include_once "./assets/pages/require/header.php";
                     include_once './assets/posts/functions/'.$page.'/'.$pageitem.'.php';
+                    include_once "./assets/pages/require/footer.php";
                 }
             } else {
                 // Check if there is a basis post functions for that function
                 if(file_exists('./assets/posts/functions/'.$page.'/basis.php')) {
+                    include_once "./assets/pages/require/header.php";
                     include_once './assets/posts/functions/'.$page.'/basis.php';
+                    include_once "./assets/pages/require/footer.php";
                 }
             }
         } else {
             // Check for single page POST requests.
             if(file_exists("./assets/posts/single/$page.php")) {
+                include_once "./assets/pages/require/header.php";
                 include_once "./assets/posts/single/$page.php";
+                include_once "./assets/pages/require/footer.php";
             }
         }
     }
