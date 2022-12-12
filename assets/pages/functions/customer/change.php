47c01279135af88c488bbb0766673629829a6355
Zerdardian | Kjeld Jense <legokjeld@hotmail.com>
2022-12-12 10:27:00 +0100

    Created all and start change delete

---
+++ assets/pages/functions/customer/change.php
@@ -0,0 +1,12 @@
+<?php
+$seller = $db->query("SELECT sellerid FROM `seller` WHERE `userid`='$userid'")->fetch();
+$sellerid = $seller['sellerid'];
+$comicid = $subitem;
+$comic = $db->query("SELECT * FROM `comic` WHERE `comicid`='$comicid' AND `sellerid`='$sellerid'")->fetch();
+$owners = $db->query("SELECT * FROM `owned` WHERE `comicid`='$comicid'")->fetchAll();
+
+$people = 0;
+
+foreach($owners as $owner) {
+    $people++;
+}
