217f4c8e683afd2b6f6b36c64fb2ac8a06a7811a
Zerdardian | Kjeld Jense <legokjeld@hotmail.com>
2022-12-13 23:12:41 +0100

    added delete.

--- assets/pages/functions/customer/delete.php
+++ assets/pages/functions/customer/delete.php
@@ -10,6 +10,18 @@
     foreach($owners as $owner) {
         $people++;
     }
-
-    print_r($comic);
 ?>
+<div class="areyousure deletecomic">
+    <div class="image">
+        <img src="/assets/images/comics/<?=$comic['comicpicture']?>" alt="">
+    </div>
+    <div class="removecomic">
+        <h2>Are you sure?</h2>
+        <p>Are you sure you want to remove <?=$comic['comicname']?>?<br>
+        This action cannot be reversed as this will remove it for you and all <?=$people?> users.</p>
+        <form action="/customer/delete/<?=$comicid?>/" method="post">
+            <input type="hidden" name="remove" value=true>
+            <input type="submit" value="Remove comic">
+        </form>
+    </div>
+</div>
