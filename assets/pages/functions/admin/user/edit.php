692c41d3acaa1359da9b3fefad25244208cbe53a
Zerdardian | Kjeld Jense <legokjeld@hotmail.com>
2022-12-20 22:25:28 +0100

    added start admin

---
+++ assets/pages/functions/admin/user/edit.php
@@ -0,0 +1,55 @@
+<?php
+    $pageid = $pagesubitem;
+    $usertype[1] = "User";
+    $usertype[2] = "Seller";
+    $usertype[3] = "Admin";
+    $user = $db->query("SELECT user.userid, user.email, user.created, user.lastlogin, userinfo.usertype, userinfo.firstname, userinfo.lastname FROM user, userinfo WHERE user.userid = $pageid AND userinfo.userid = $pageid")->fetch();
+    if($userid == $user['userid']) {
+        $selectbox = 'disabled';
+    } else {
+        $selectbox = '';
+    }
+    if(!empty($user)) {
+        ?>
+            <div class="form">
+                <form action="/admin/users/edit/<?=$user['userid']?>/" method="post">
+                    <input type="hidden" name="userid" value=<?=$user['userid']?>>
+                    <label for="email">Email<?php
+                        if($user['userid'] == $userid) {
+                            ?> | You cannot edit your email here!<?php
+                        }
+                    ?></label>
+                    <input type="text" name="email" id="email" value="<?=$user['email']?>" <?=$selectbox?>>
+                    <label for="Usertype">Usertype<?php
+                        if($user['userid'] == $userid) {
+                            ?> | You cannot edit your own rank!<?php
+                        }
+                    ?></label>
+                    <select name="usertype" id="usertype" <?=$selectbox?>>
+                        <?php
+                            foreach($usertype as $key=>$val) {
+                                if($key == $user['usertype']) {
+                                    $selected = "selected='true'";
+                                } else {
+                                    $selected = "";
+                                }
+                                ?>
+                                <option value="<?=$key?>" <?=$selected?>><?=$val?></option>
+                                <?php
+                            }
+                        ?>
+                    </select>
+                    <label for="firstname">Firstname</label>
+                    <input type="text" name="firstname" id="firstname" value="<?=$user['firstname']?>">
+                    <label for="lastname">Lastname</label>
+                    <input type="text" name="lastname" id="lastname" value="<?=$user['lastname']?>">
+                    <input type="submit" value="Edit">
+                </form>
+            </div>
+        <?php
+    } else {
+        ?>
+            No User found. Please select a user on the user page.
+        <?php
+    }
+?>
---
+++ assets/pages/functions/menus/adminmenu.php
@@ -0,0 +1,22 @@
+<div class="menu">
+    <a href="/admin/home/">
+        <div class="menuitem home">
+            Home
+        </div>
+    </a>
+    <a href="/admin/users/">
+        <div class="menuitem New">
+            Users
+        </div>
+    </a>
+    <a href="/customer/comics/">
+        <div class="menuitem all">
+            Comics
+        </div>
+    </a>
+    <a href="/customer/userinfo/">
+        <div class="menuitem userinfo">
+            Userinfo
+        </div>
+    </a>
+</div>
