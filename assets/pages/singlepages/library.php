10e0bc32aed095ab830d7e43019bc3a8749e56ef
IrisDeBlok <99642080+IrisDeBlok@users.noreply.github.com>
2022-12-21 10:47:12 +0100

    edit css and basic changes

---
+++ assets/pages/singlepages/library.php
@@ -0,0 +1,17 @@
+<div class="myLibrary" id="myLibrary">
+    <div class="pagetitle"></div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Titel</th>
+                    <th>Beschrijving</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
+    <div class="pagetitle"></div>
+</div>
